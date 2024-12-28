d44abf8b5631edf0f209094b108a5560c540bcc5
7xrk <161611272+7xrk@users.noreply.github.com>
2024-12-28 20:52:32 +0900

    node example

---
+++ examples/node/example_node.ts
@@ -0,0 +1,34 @@
+import { outdent } from "outdent";
+// import { load } from "https://deno.land/std@0.217.0/dotenv/mod.ts";
+import {
+  NovelAIDiffusionModels,
+  NovelAISession,
+  generateImage,
+  NovelAIImageSizePreset,
+  NovelAIImageUCPresetType,
+} from "@7xrk/novelai-api";
+
+const session = await NovelAISession.login("email", "password");
+
+Deno.mkdirSync("./tmp", { recursive: true });
+
+console.log("generating");
+const result = await generateImage(session, {
+  limitToFreeInOpus: true,
+  prompt: outdent`1girl,{{{best quality, amazing quality, very aesthetic}}}`,
+  undesiredContent: outdent`worst quality`,
+  ucPreset: NovelAIImageUCPresetType.Heavy,
+  model: NovelAIDiffusionModels.NAIDiffusionAnimeV3,
+  size: NovelAIImageSizePreset.NORMAL_LANDSCAPE,
+  smea: { dyn: true },
+  img2img: {
+    keepAspect: true,
+    image: Deno.readFileSync("./test.jpeg"),
+    strength: 0.8,
+  },
+});
+
+for (const file of result.files) {
+  const bin = await file.arrayBuffer();
+  Deno.writeFileSync(`./tmp/test.png`, new Uint8Array(bin));
+}
